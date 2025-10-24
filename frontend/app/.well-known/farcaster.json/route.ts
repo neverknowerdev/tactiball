5483431fc0e420fec366a056975fc00db8aa435e
NeverKnower <neverknowerdev@gmail.com>
2025-10-25 01:52:15 +0200

    updated app version in farcaster.json

--- frontend/app/.well-known/farcaster.json/route.ts
+++ frontend/app/.well-known/farcaster.json/route.ts
@@ -21,7 +21,7 @@ export async function GET() {
       signature: "MHgyMjI5MWVlNzRiNjMyMTQzMDg0ZTlmNjljMjE1MDdiODE3NmI4NTMxMTQ2YjUxMmQyOWQ4MDkwNGU2ZWU5MzgyNDY3NzJiOTFhNWUxNGVjOTM1MTg0ZTVlNTg5ZmNiNTVjZjc0MmZiYzgwNDE0NGJiZGRjMGIxNTJlMTMwZjg4ODFj"
     },
     frame: withValidProperties({
-      version: "1",
+      version: "2",
       name: "TactiBall",
       subtitle: "Play. Bet. Earn.",
       description: "Play tactical football - 2D board game, where tactic meets probability. Build your own team, play against friends and earn rewards.",
